c81a4e7416509eeb9aab2b9f3730e9c258aa7838
reece-elder <reece.elder@qa.com>
2021-10-19 10:48:54 +0100

    added new project

---
+++ newNewProject/src/com/qa/main/Runner.java
@@ -0,0 +1,17 @@
+package com.qa.main;
+
+public class Runner {
+	
+	// Main method
+	// Ctrl Space opens up the auto complete window
+	public static void main(String[] args) {
+		// All code runs from Main top to bottom
+		System.out.println("Hello everyone!");
+		System.out.println("syso ctrl space"); // syso ctrl space -> System out
+	}
+	
+	// public -> anything in this package com.qa.main can see and use this
+	// static -> something that has to be there 
+	// void -> no return type
+
+}

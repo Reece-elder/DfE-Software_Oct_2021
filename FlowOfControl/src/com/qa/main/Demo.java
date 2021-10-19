714d367f743996d8ae2de2453f267f3023c612b0
reece-elder <reece.elder@qa.com>
2021-10-19 11:29:21 +0100

    added flow of control

---
+++ FlowOfControl/src/com/qa/main/Demo.java
@@ -0,0 +1,13 @@
+package com.qa.main;
+
+public class Demo {
+	
+	// This class will contain the logic, Runner will run the methods from this class
+	
+	// Creating a method that prints out "im first"
+	
+	public static void imFirst() {
+		System.out.println("I'm first!");
+	}
+
+}
---
+++ FlowOfControl/src/com/qa/main/Runner.java
@@ -0,0 +1,42 @@
+package com.qa.main;
+
+public class Runner {
+	
+	// main method is what runs
+	// dont write code in main, link to methods
+	public static void main(String[] args) {
+		
+//		// Tell my main method to run newMethod
+//		// type in the name of the method to run + () + ;
+//		newMethod();
+//		helloDeacon();
+//		
+//		// Code runs FROM main, top to bottom
+//		System.out.println("3");
+//		System.out.println("2");
+//		System.out.println("1");
+		
+		// Reference the class . the method
+		Demo.imFirst();
+		
+	}
+	
+	// make a new method called helloDeacon
+	
+	public static void helloDeacon() {
+		System.out.println("Hello Deacon!");
+	}
+	
+	// New method
+	// All methods SHOULD be public static void (but this will change)
+	public static void newMethod() {
+		// method body - what runs when we say 'Run this method'
+		System.out.println("Hello World!");
+		
+	}
+	
+
+	
+	
+
+}
